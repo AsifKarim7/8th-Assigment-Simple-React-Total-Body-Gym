dfe9131f57fa90068aa7c6e7044bc47d40c26451
AsifKarim7 <asifkarim275@gmail.com>
2022-09-29 18:09:27 +0600

    blog q3 done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -12,6 +12,17 @@ const Blogs = () => {
                 <h2>What are the difference between props and state?</h2>
                 <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
             </div>
+            <div className='blog-container'>
+                <h2>What are the usage of useEffect except data loading in react-js?</h2>
+                <p>There are five more cases to use useEffect :-
+                    <ol>
+                        <li>Running on state change: validating input field.</li>
+                        <li>Running on state change: live filtering.</li>
+                        <li>Running on state change: trigger animation on new array value.</li>
+                        <li>Running on props change: update paragraph list on fetched API data update.</li>
+                        <li>Running on props change: updating fetched API data to get BTC updated price.</li>
+                    </ol></p>
+            </div>
         </div>
     );
 };
