e1ae29af79f905d6cd0cfb132c0ec162f811bb74
AsifKarim7 <asifkarim275@gmail.com>
2022-09-29 18:04:47 +0600

    blog q2 done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -8,6 +8,10 @@ const Blogs = () => {
                 <h2>How does react work?</h2>
                 <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
             </div>
+            <div className='blog-container'>
+                <h2>What are the difference between props and state?</h2>
+                <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
+            </div>
         </div>
     );
 };
